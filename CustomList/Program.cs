fb8af0c5fb4e3d037fd6fce878e55ffd488d4a4c
ArmanPoghosyan2001 <a.poghosyan3001@gmail.com>
2021-09-30 22:17:46 +0400

    Add project files.

---
+++ CustomList/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CustomList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    
+}
